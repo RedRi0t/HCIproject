0b0d27dfa84ce7b9f05df79e7ee0dd72accfbe9c
julia98 <925583000@qq.com>
2019-05-23 15:21:01 +0800

    searchList update

--- src/views/reader/SearchList.vue
+++ src/views/reader/SearchList.vue
@@ -7,13 +7,14 @@
             <el-col :span="8">
                 <br/>
                 <div style="margin-top: 15px;">
-                    <el-input placeholder="请输入内容">
+                    <el-input v-model="searchContent" placeholder="请输入内容">
                         <!--<el-select v-model="select" slot="prepend" placeholder="请选择">-->
                         <!--<el-option label="餐厅名" value="1"></el-option>-->
                         <!--<el-option label="订单号" value="2"></el-option>-->
                         <!--<el-option label="用户电话" value="3"></el-option>-->
                         <!--</el-select>-->
-                        <el-button slot="append" icon="el-icon-search"></el-button>
+                        <el-button slot="append" icon="el-icon-search"
+                        @click="searchRes()"></el-button>
                     </el-input>
                 </div>
             </el-col>
@@ -27,18 +28,16 @@
                 <div v-for="i in 4" :key="i">
                     <el-card class="box-card">
                         <div slot="header">
-                            <span>Mc Donalds</span>
-                            <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+                            <span>{{res_name}}</span>
+                            <el-button style="float: right; padding: 3px 0" type="text"
+                                       @click="resDetailPage()">查看详细</el-button>
                         </div>
                         <el-col :span="8">
                             <img src="./img/hamburger.jpeg" class="image"/>
                         </el-col>
                         <el-col :span="16">
                             <div style="border-right: 15px;">
-                                <span>好吃的汉堡</span>
-                                <div class="bottom">
-                                    <el-button type="text" class="button">操作按钮</el-button>
-                                </div>
+                                <span>{{description}}</span>
                             </div>
                         </el-col>
                     </el-card>
@@ -56,11 +55,29 @@
 
     @Component({})
     export default class SearchList extends Vue{
-        name: "SearchList"
+        name: "SearchList";
+        searchContent:String="";
+        resInfoList:RestaurantInfo[]=[];
+
+        res_name:String="Mc Donalds";
+        description:String="汉堡汉堡，好吃的汉堡。";
+        address:String="就在世界的每个角落";
+        image:String="";
+
+        private searchRes() {
+            axios.get('http://localhost:3142/search/list/'+this.searchContent).then((response)=>{
+
+                this.resInfoList=response.data;
+            })
+        }
+
+        private resDetailPage() {
+
+        }
     }
 
     class RestaurantInfo{
-        name:String="Mc Donalds";
+        res_name:String="Mc Donalds";
         description:String="汉堡汉堡，好吃的汉堡。";
         address:String="就在世界的每个角落";
         image:String="";
